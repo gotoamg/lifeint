4ae181affe669d6cf6fd5ea7a38d131f7afbcdaf
gotoamg <61301390+gotoamg@users.noreply.github.com>
2026-04-22 21:43:07 -0400

    Update site - 2026-04-23T01:43:07.475Z

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,13 @@ export default {
   darkMode: ["class"],
   content: ["./index.html", "./src/**/*.{ts,tsx}"],
   prefix: "",
+  safelist: [
+    "max-md:!hidden",
+    "max-lg:md:!hidden",
+    "lg:!hidden",
+    "!hidden",
+    "hidden",
+  ],
   theme: {
     container: { center: true, padding: "2rem", screens: { "2xl": "1400px" } },
     extend: {
